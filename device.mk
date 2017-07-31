ee21619ecf64de60a72a8fe431db531c1ddae0d6
hak86 <hakamelassouad@gmail.com>
2017-07-31 02:29:58 +0200

    prague: build zygote64_32

--- device.mk
+++ device.mk
@@ -154,3 +154,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     	dalvik.vm.heapsize=512m \
     	dalvik.vm.heaptargetutilization=0.75 \
     	dalvik.vm.heapstartsize=8m \
+
+# Zygote
+ADDITIONAL_DEFAULT_PROPERTIES += \
+	ro.zygote=zygote64_32
+
+PRODUCT_COPY_FILES += \
+	system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.zygote=zygote64_32
