6a5957c24b905e8f19636387f44ad3b254189c73
ha86 <hakamelassouad@gmail.com>
2017-09-01 00:05:54 +0200

    prague: update HwHisiRIL class thanks to OpenKirin

--- ril/telephony/java/com/android/internal/telephony/HwHisiRIL.java
+++ ril/telephony/java/com/android/internal/telephony/HwHisiRIL.java
@@ -3,7 +3,7 @@
  * Not a Contribution.
  *
  * Copyright (C) 2006 The Android Open Source Project
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
