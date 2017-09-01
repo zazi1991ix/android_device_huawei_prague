9c134162b118f6d66402faecd0e4bfcd965b4d3e
ha86 <hakamelassouad@gmail.com>
2017-09-01 11:54:24 +0200

    prague: lineage -> aosp

--- aosp_prague.mk
+++ aosp_prague.mk
@@ -26,7 +26,7 @@
 # Device
 $(call inherit-product, device/huawei/prague/full_prague.mk)
 
-PRODUCT_NAME := lineage_prague
+PRODUCT_NAME := aosp_prague
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
@@ -34,6 +34,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-huawei
 # Display
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-
-# LineageOS 14.1
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
--- device.mk
+++ device.mk
@@ -65,8 +65,27 @@ PRODUCT_COPY_FILES += \
 
 # Dalvik
 PRODUCT_TAGS += dalvik.gc.type-precise
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    	dalvik.vm.heapstartsize=8m \
+    	dalvik.vm.heapgrowthlimit=288m \
+    	dalvik.vm.heapsize=768m \
+    	dalvik.vm.heaptargetutilization=0.75 \
+    	dalvik.vm.heapminfree=512k \
+    	dalvik.vm.heapmaxfree=8m
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    	ro.hwui.texture_cache_size=72 \
+    	ro.hwui.layer_cache_size=48 \
+    	ro.hwui.path_cache_size=32 \
+    	ro.hwui.gradient_cache_size=1 \
+    	ro.hwui.drop_shadow_cache_size=6 \
+    	ro.hwui.r_buffer_cache_size=8 \
+    	ro.hwui.texture_cache_flushrate=0.4 \
+    	ro.hwui.text_small_cache_width=1024 \
+    	ro.hwui.text_small_cache_height=1024 \
+    	ro.hwui.text_large_cache_width=2048 \
+    	ro.hwui.text_large_cache_height=1024
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
